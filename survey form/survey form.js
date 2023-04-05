68b3d9d780e1bb2a0dcc694741a457da90d3ae26
GRACE0211 <110452979+GRACE0211@users.noreply.github.com>
2023-04-05 14:07:57 +0800

    Update survey form.js

--- survey form/survey form.js
+++ survey form/survey form.js
@@ -1,13 +1,24 @@
 const app = Vue.createApp({
   data(){
     return {
+      userInfo:{
       name:"",
       email:"",
       age:"",
       numberOfVisits: "first-time",
       userRecommend: "definitely",
       reasonsToVisitAgain:[],
-      improvedReasons:[]
+      improvedReasons:[],
+      comments:""
+      }
     }
-  }
+  },
+  methods: {
+    demo(){
+      if(confirm('是否提交表單？')){
+        console.log(JSON.stringify(this.userInfo));
+        // this.userInfo.reset();
+      }
+    }
+  },
 }).mount("#app");
